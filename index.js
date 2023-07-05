b36837c66f587a9166f8fe062a41a36599eb825c
Rahul Patwal <rahul.oneto11@gmail.com>
2023-07-05 12:17:27 +0530

    basic setup

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
+const connect = require('./config/db-config');
 const io = new Server(server);
+
+app.set('view engine', 'ejs');
 
 // middleware
 app.use('/', express.static(__dirname + '/public'));
@@ -11,8 +14,6 @@ app.use('/', express.static(__dirname + '/public'));
 
 io.on('connection', (socket) =>{
     console.log("A user is connected..", socket.id);
-    // console.log("A user is connected..", socket);
-    // console.log("A user is connected..", socket.id);
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id);
       });
@@ -20,25 +21,32 @@ io.on('connection', (socket) =>{
     //   socket.on('from_client', () =>{
     //     console.log("recieved event from cleint");
     //   })
-
     //   setInterval(function f(){
     //     socket.emit('from_server');
     //   }, 3000)
+
+    socket.on("join_room", (data) =>{
+      console.log("joining a room", data.roomid);
+      socket.join(data.roomid);
+    })
 
     socket.on('new_message', (data) =>{
       //  io.emit('msg_recieved', data) // it send to all
         // to send same client
         //socket.emit('msg_recieved', data) //to particular
-        socket.broadcast.emit('msg_recieved', data) //to particular
-
+       // socket.broadcast.emit('msg_recieved', data) //to particular
+        io.to(data.roomid).emit('msg_recieved', data);
     })
 })
 
 
+app.get('/chat/:roomid/:user', async(req , res) =>{
+  res.render('index', {roomid : req.params.roomid, user: req.params.user})
+})
 
 
-
-
-server.listen(3000, () => {
+server.listen(3000, async() => {
   console.log('listening on *:3000');
+  // await connect();
+  console.log("DB Connected ..")
 });
