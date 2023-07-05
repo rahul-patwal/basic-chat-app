21de6c02481a02ada16875eda1e506f646cea5e7
Rahul Patwal <rahul.oneto11@gmail.com>
2023-07-05 13:43:46 +0530

    some random code

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const connect = require('./config/db-config');
 const io = new Server(server);
-
+// app.use('/', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 // middleware
-app.use('/', express.static(__dirname + '/public'));
 
 
 io.on('connection', (socket) =>{
     console.log("A user is connected..", socket.id);
+
     socket.on('disconnect', () => {
         console.log('user disconnected', socket.id);
       });
@@ -26,6 +27,7 @@ io.on('connection', (socket) =>{
     //   }, 3000)
 
     socket.on("join_room", (data) =>{
+      console.log("inside join_room ========== ", data);
       console.log("joining a room", data.roomid);
       socket.join(data.roomid);
     })
@@ -42,6 +44,14 @@ io.on('connection', (socket) =>{
 
 app.get('/chat/:roomid/:user', async(req , res) =>{
   res.render('index', {roomid : req.params.roomid, user: req.params.user})
+})
+
+app.get('/group', async(req , res) =>{
+  res.render('group')
+})
+
+app.post('/group', async(req , res) =>{
+     console.log(req.body)
 })
 
 
---
+++ models/group.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+    name:{
+        type: String
+    },
+    isPersonal:{
+        type: Boolean,
+        default: false,
+    }
+  
+})
+
+const group = mongoose.model('group', groupSchema);
+
+module.exports = group
